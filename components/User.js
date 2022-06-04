f2cb75830e94baf67e7f019413f324de7a9e0397
Digital Mind <jremesz@gmail.com>
2022-06-04 17:37:06 +0200

    authentication with google finished as well as styling of all components  and pages

--- components/User.js
+++ components/User.js
@@ -1,3 +1,28 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function User() {
-  return <div>User</div>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <p className="link">
+          <img
+            onClick={signOut}
+            src={session.user.image}
+            alt="user-image-or-avatar"
+            className="h-10 w-10 rounded-full hover:bg-gray-400 cursor-pointer p-1"
+          />
+        </p>
+      </>
+    );
+  }
+  return (
+    <>
+      <button
+        onClick={signIn}
+        className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-110 hover:shadow-md "
+      >
+        Sign in
+      </button>
+    </>
+  );
 }
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Header from "../../components/Header";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
-export default function signIn({ providers }) {
+export default function signin({ providers }) {
   return (
     <>
       <Header />
-      <div className="">
+      <div className="mt-40">
         {Object.values(providers).map((provider) => (
-          <div key={provider.name} className="">
-            <img src="/google-logo.svg" alt="Google Logo" />
-            <p>This website is created for learning purposes</p>
-            <button>Sign in with {provider.name}</button>
+          <div key={provider.name} className="flex flex-col items-center">
+            <img
+              className="w-24 object-cover"
+              src="/google-logo.svg"
+              alt="Google Logo"
+            />
+            <p className="text-sm italic my-10 text-center">
+              This website is created for learning purposes
+            </p>
+            <button
+              className="bg-red-500 text-white px-5 py-3 rounded-lg font-medium hover:bg-red-600"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name}
+            </button>
           </div>
         ))}
       </div>
