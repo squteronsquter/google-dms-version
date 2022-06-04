f0fa9aa3d53620583855b1e686190f944545bafe
Digital Mind <jremesz@gmail.com>
2022-06-04 19:37:49 +0200

    body section with search form and icons created

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Image from "next/image";
+import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -16,6 +18,29 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Body */}
+      <form className="flex flex-col items-center mt-40">
+        <Image
+          width="100"
+          objectFit="cover"
+          height="100"
+          src="/google-logo.svg"
+          alt="Google Logo"
+        />
+        <div className="mt-12 mb-12 flex w-full m-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 text-gray-500 mr-3" />
+          <input
+            type="text"
+            name=""
+            id=""
+            className="flex-grow focus:outline-none"
+          />
+          <MicrophoneIcon className="h-5 text-gray-500" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+      </form>
 
       {/* Footer */}
     </>
